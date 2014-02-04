75a4ca68e2a4f21cbc97134b612c819e0b1589c2
Mike Harrison <mharrison@netmediagroup.com>
2014-02-04 11:48:59 -0700

    Changing database strings

--- includes/constants.php
+++ includes/constants.php
@@ -1,7 +1,7 @@
 <?php
 	//Define constants here
 	define('DB_SERVER', 'localhost');
-	define('DB_USER', 'spincent_admin');
-	define('DB_PASSWORD', 'Justhost!984');
-	define('DB_NAME','spincent_school');
+	define('DB_USER', 'root');
+	define('DB_PASSWORD', 'Database84!');
+	define('DB_NAME','users');
 ?>
